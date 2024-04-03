478acc9db8169c1be4d46d6b152bbb5c1a4a7696
PREMP225 <prem01.monarch@gmail.com>
2024-04-03 12:50:10 +0530

    CREATE NEW FLUTTER PROJECT

---
+++ android/app/src/main/kotlin/com/example/camraview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.camraview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,75 @@
+import 'dart:ffi';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+class ShowCamraView extends StatefulWidget {
+  const ShowCamraView({super.key});
+
+  @override
+  State<ShowCamraView> createState() => _ShowCamraViewState();
+}
+
+class _ShowCamraViewState extends State<ShowCamraView> {
+
+  CameraController? cameraController;
+late List<CameraDescription>  cameras;
+
+
+void startCamra()async{
+cameras  = await availableCameras();
+
+cameraController = CameraController(cameras.first, ResolutionPreset.medium,enableAudio: false);
+ 
+ await cameraController!.initialize().then((value){
+  if(!mounted){
+    return;
+
+  }
+  setState(() {
+    
+  });
+ });
+ }
+
+
+
+ @override
+  void dispose() {
+    cameraController!.dispose();
+    super.dispose();
+  }
+  
+
+  @override
+  void initState() {
+    super.initState();
+ startCamra();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+
+if(cameraController != null &&  cameraController!.value.isInitialized){
+
+  return Scaffold(
+    body: Column(
+      children: [
+        CameraPreview(cameraController!)
+      ],
+    ),
+  );
+}else{
+  return Container(
+    height: 500,
+    width: MediaQuery.of(context).size.width,
+    child: Center(
+      child: CircularProgressIndicator(color: Colors.blue,),
+    ),
+  );
+}
+
+  
+  }
+}
